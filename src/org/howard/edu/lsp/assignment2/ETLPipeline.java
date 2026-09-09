7ebd8c617650e2d713daae241f1bf11e93dd8830
Tahmauri <tmbo718@gmail.com>
2026-09-09 04:14:40 -0400

    added list for record, added reader to skip header

--- src/org/howard/edu/lsp/assignment2/ETLPipeline.java
+++ src/org/howard/edu/lsp/assignment2/ETLPipeline.java
@@ -1,5 +1,10 @@
 package org.howard.edu.lsp.assignment2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ETLPipeline {
     private static final String INPUT_PATH = "data/employees.csv";
     private static final String OUTPUT_PATH = "data/transformed_employees.csv";
@@ -8,6 +13,23 @@ public class ETLPipeline {
         
         int rowsRead = 0;
         int rowsSkipped = 0;
+
+        List<String[]> rawRecords = new ArrayList<>();
+
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(INPUT_PATH))) {
+            String line;
+            boolean isHeader = true;
+
+
+            while ((line = reader.readLine()) != null) {
+                if (isHeader) {
+                    isHeader = false;
+                    continue;
+                }
+
+                    rowsRead++;
+
 
     }
 
