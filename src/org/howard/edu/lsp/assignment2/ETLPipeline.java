180e22fac4bca5fc407a8b4c169656465ef6672f
Tahmauri <tmbo718@gmail.com>
2026-09-09 05:05:52 -0400

    added math imports

--- src/org/howard/edu/lsp/assignment2/ETLPipeline.java
+++ src/org/howard/edu/lsp/assignment2/ETLPipeline.java
@@ -2,8 +2,10 @@ package org.howard.edu.lsp.assignment2;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class ETLPipeline {
     private static final String INPUT_PATH = "data/employees.csv";
