a4e97b49b7419a126ad9c1db3d64763ef0952a6b
Tahmauri <tmbo718@gmail.com>
2026-09-23 03:57:30 -0400

    now writes record to csv file

--- src/org/howard/edu/lsp/assignment3/Payrollcsvwriter.java
+++ src/org/howard/edu/lsp/assignment3/Payrollcsvwriter.java
@@ -1,5 +1,10 @@
 package org.howard.edu.lsp.assignment3;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 public class Payrollcsvwriter {
     private final String path;
  
@@ -11,4 +16,14 @@ public class Payrollcsvwriter {
         return path;
     }
 
+    public void write(List<Payrollrecord> records) throws IOException {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(path))) {
+            writer.println(Payrollrecord.CSV_HEADER);
+            for (Payrollrecord record : records) {
+                writer.println(record.toCsvRow());
+            }
+        }
+
+
+    }
 }
