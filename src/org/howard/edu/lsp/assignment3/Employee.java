df12bf121d538070ab8186bee9ebd8ac2d31b5b4
Tahmauri <tmbo718@gmail.com>
2026-09-23 03:22:54 -0400

    Created employee based on input file

--- src/org/howard/edu/lsp/assignment3/Employee.java
+++ src/org/howard/edu/lsp/assignment3/Employee.java
@@ -1,5 +1,26 @@
 package org.howard.edu.lsp.assignment3;
 
 public class Employee {
-    
+    private final int employeeId;
+    private final String name;
+    private final String department;
+    private final double hoursWorked;
+    private final double hourlyRate;
+
+    public Employee(int employeeId, String name, String department,
+                    double hoursWorked, double hourlyRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getEmployeeId() { return employeeId; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public double getHoursWorked() { return hoursWorked; }
+    public double getHourlyRate() { return hourlyRate; }
+
+
 }
