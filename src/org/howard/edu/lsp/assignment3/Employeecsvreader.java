9f3615d01c1b9db53cd69ba2d7bfd95fbacf139c
Tahmauri <tmbo718@gmail.com>
2026-09-23 03:11:24 -0400

    finished csv reader, started parser

--- src/org/howard/edu/lsp/assignment3/Employeecsvreader.java
+++ src/org/howard/edu/lsp/assignment3/Employeecsvreader.java
@@ -1,5 +1,11 @@
 package org.howard.edu.lsp.assignment3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employeecsvreader {
     private final String path;
  
@@ -9,5 +15,17 @@ public class Employeecsvreader {
  
     public String getPath() {
         return path;
+    }
+
+    public List<String> readDataLines() throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            reader.readLine();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        return lines;
     }
 }
--- src/org/howard/edu/lsp/assignment3/Employeeparser.java
+++ src/org/howard/edu/lsp/assignment3/Employeeparser.java
@@ -3,5 +3,6 @@ package org.howard.edu.lsp.assignment3;
 import java.util.Optional;
 
 public class Employeeparser {
-    
+    private static final int EXPECTED_FIELDS = 5;
+
 }
