500ef94cd843b8f8f6464d898ce71761a38f836b
Tahmauri <tmbo718@gmail.com>
2026-09-23 03:40:26 -0400

    employee status type added

--- src/org/howard/edu/lsp/assignment3/Employmentstatus.java
+++ src/org/howard/edu/lsp/assignment3/Employmentstatus.java
@@ -1,5 +1,23 @@
 package org.howard.edu.lsp.assignment3;
 
 public class Employmentstatus {
-    
+    PART_TIME("Part-Time"),
+    FULL_TIME("Full-Time");
+
+    private static final double FULL_TIME_THRESHOLD = 30.00;
+ 
+    private final String label;
+
+    EmploymentStatus(String label) {
+        this.label = label;
+    }
+
+    public static EmploymentStatus fromHours(double hoursWorked) {
+        return (hoursWorked < FULL_TIME_THRESHOLD) ? PART_TIME : FULL_TIME;
+    }
+ 
+    @Override
+    public String toString() {
+        return label;
+    }
 }
