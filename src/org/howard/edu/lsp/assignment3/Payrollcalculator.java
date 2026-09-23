79132c35995d97b19890fabeedb89cfc97db0beb
Tahmauri <tmbo718@gmail.com>
2026-09-23 03:26:12 -0400

    started building transformed records for employees

--- src/org/howard/edu/lsp/assignment3/Payrollcalculator.java
+++ src/org/howard/edu/lsp/assignment3/Payrollcalculator.java
@@ -4,5 +4,18 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class Payrollcalculator {
-    
+    private static final double OVERTIME_THRESHOLD = 40.00;
+    private static final double OVERTIME_MULTIPLIER = 1.5;
+    private static final String BONUS_DEPARTMENT = "IT";
+    private static final double IT_BONUS_RATE = 0.05;
+
+    public Payrollrecord process(Employee employee) {
+        BigDecimal grossPay = calculateGrossPay(employee);
+        return new Payrollrecord(
+                employee,
+                grossPay,
+                PayLevel.fromGrossPay(grossPay),
+                EmploymentStatus.fromHours(employee.getHoursWorked()));
+    }
+
 }
