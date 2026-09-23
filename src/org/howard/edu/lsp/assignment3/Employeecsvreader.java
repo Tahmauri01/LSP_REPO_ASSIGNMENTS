481cf9243348a1d52003d8dba6841e0673bdffa2
Tahmauri <tmbo718@gmail.com>
2026-09-23 03:07:00 -0400

    started coding the reading of the input file

--- src/org/howard/edu/lsp/assignment3/Employeecsvreader.java
+++ src/org/howard/edu/lsp/assignment3/Employeecsvreader.java
@@ -1,11 +1,13 @@
 package org.howard.edu.lsp.assignment3;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Employeecsvreader {
-
+    private final String path;
+ 
+    public Employeecsvreader(String path) {
+        this.path = path;
+    }
+ 
+    public String getPath() {
+        return path;
+    }
 }
